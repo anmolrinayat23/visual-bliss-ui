d96e0bdce9098e426eaa138337a51fc3973a90e5
vikas rahangdale <vikasrahangdale5606@gmail.com>
2025-11-03 00:55:11 +0530

    added imges

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -129,7 +129,7 @@ const Courses = ({ showFooter = true }) => {
         "Advanced computing program with AI, machine learning, cybersecurity, and full-stack development.",
       buttonText: "Apply Now",
       category: "Computer",
-      image: "/public/mca.png"
+      image: "/mca.png"
     },
     {
       title: "MA",
