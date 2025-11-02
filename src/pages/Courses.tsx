ad435e65e680efe1809731e27969f08a2cf21e23
vikas rahangdale <vikasrahangdale5606@gmail.com>
2025-11-03 00:56:55 +0530

    added imges

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -121,7 +121,7 @@ const Courses = ({ showFooter = true }) => {
         "IT program developing expertise in programming, software design, and database management.",
       buttonText: "Apply Now",
       category: "Computer",
-      image: "/public/bca.png"
+      image: "/bca.png"
     },
     {
       title: "MCA",
