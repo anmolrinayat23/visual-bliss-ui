cc5af616093a696041a70bb866d6a5395eb9ee03
vikas rahangdale <vikasrahangdale5606@gmail.com>
2025-11-03 01:07:35 +0530

    added imges

--- src/pages/BookSession.tsx
+++ src/pages/BookSession.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import counsellingImage from "@/assets/counselling.jpg";
+import counsellingImage from "@/assets/BookCounselling.png";
 import { Calendar, Clock, User, Phone, GraduationCap } from "lucide-react";
 
 const BookSession = ({showFooter=true}) => {
